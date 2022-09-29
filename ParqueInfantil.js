46fae0196209c6e7bc00e413e8a5670fea6ad46a
yessmsr <yesseniasolano008@gmail.com>
2022-09-29 17:32:46 -0600

    Create ParqueInfantil.js

---
+++ ParqueInfantil.js
@@ -0,0 +1,8 @@
+var ParqueInfantil = {
+"type": "FeatureCollection",
+"name": "ParqueInfantil",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "cat": 81, "osm_id": null, "osm_way_id": "1097156710", "name": "Parque infantil", "type": null, "aeroway": null, "amenity": null, "admin_level": null, "barrier": null, "boundary": null, "building": null, "craft": null, "geological": null, "historic": null, "land_area": null, "landuse": null, "leisure": "park", "man_made": null, "military": null, "natural": null, "office": null, "place": null, "shop": null, "sport": null, "tourism": null, "other_tags": "\"addr:city\"=>\"Cartago\",\"addr:street\"=>\"Diagonal 10|\"" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -83.9099302, 9.8593414 ], [ -83.9097578, 9.8592126 ], [ -83.9097525, 9.8592093 ], [ -83.9097149, 9.8592575 ], [ -83.9098879, 9.8593903 ], [ -83.9099302, 9.8593414 ] ] ] } }
+]
+};
