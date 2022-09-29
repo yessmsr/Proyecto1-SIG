5fc331f8174c2dd71afe177fdd13a15d3e655eec
yessmsr <yesseniasolano008@gmail.com>
2022-09-29 17:32:23 -0600

    Create Hidrantes.js

---
+++ Hidrantes.js
@@ -0,0 +1,8 @@
+var hidrantes={
+"type": "FeatureCollection",
+"name": "Hidrantes",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "cat": 15, "osm_id": "10047990103", "name": null, "barrier": null, "highway": null, "ref": "1", "address": null, "is_in": null, "place": null, "man_made": null, "other_tags": "\"colour\"=>\"rojo\",\"emergency\"=>\"fire_hydrant\",\"fire_hydrant:type\"=>\"pillar\",\"water_source\"=>\"main\"" }, "geometry": { "type": "Point", "coordinates": [ -83.9094166, 9.8604388 ] } }
+]
+};
